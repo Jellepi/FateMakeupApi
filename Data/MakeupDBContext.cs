693fe4a87acc4aaec0a7a0e37a95006337e8350b
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-09-03 14:06:46 +0800

    Add Entity Framework Core support and initial migration
    Updated project to include Entity Framework Core and SQL Server packages.
    
    Configured database context in Program.cs and added connection string in appsettings.json.
    
    Created MakeupDBContext for database access and defined schema with initial migration.
    
    Generated necessary designer and model snapshot files for future migrations.

---
+++ Data/MakeupDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FateMakeupApi.Data
+{
+    public class MakeupDBContext(DbContextOptions<MakeupDBContext> options) : DbContext(options)
+    {
+        public DbSet<Makeup> Makeups => Set<Makeup>();
+    }
+}
